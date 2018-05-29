007e19167d173d9391650b5ae148182cd9827fe0
PaulTorchet <paul.torchet@viacesi.fr>
2018-05-29 13:43:12 +0200

    Changement de EXAMPLE en MAPS

--- model/src/main/java/model/dao/MapDAO.java
+++ model/src/main/java/model/dao/MapDAO.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.Example;
+import model.Map;
 
 /**
  * <h1>The Class ExampleDAO.</h1>
@@ -14,10 +14,11 @@ import model.Example;
  * @author Jean-Aymeric DIET jadiet@cesi.fr
  * @version 1.0
  */
-public abstract class ExampleDAO extends AbstractDAO {
+public abstract class MapDAO extends AbstractDAO {
 
+	
     /** The sql example by id. */
-    private static String sqlExampleById   = "{call findExampleById(?)}";
+    private static String sqlMapById   = "{call mapsById(?)}";
 
     /** The sql example by name. */
     private static String sqlExampleByName = "{call findExampleByName(?)}";
@@ -25,11 +26,15 @@ public abstract class ExampleDAO extends AbstractDAO {
     /** The sql all examples. */
     private static String sqlAllExamples   = "{call findAllExamples()}";
 
+    /** Affichage de la map par l'ID **/
+    //private static String sqlMapsById = "{call mapsById(?)}";
+    
+    
     /** The id column index. */
     private static int    idColumnIndex    = 1;
 
     /** The name column index. */
-    private static int    nameColumnIndex  = 2;
+    private static int    mapColumnIndex  = 2;
 
     /**
      * Gets the example by id.
@@ -40,18 +45,20 @@ public abstract class ExampleDAO extends AbstractDAO {
      * @throws SQLException
      *             the SQL exception
      */
-    public static Example getExampleById(final int id) throws SQLException {
-        final CallableStatement callStatement = prepareCall(sqlExampleById);
-        Example example = null;
+    
+    
+    public static Map getMapById(final int id) throws SQLException {
+        final CallableStatement callStatement = prepareCall(sqlMapById);
+        Map map = null;
         callStatement.setInt(1, id);
         if (callStatement.execute()) {
             final ResultSet result = callStatement.getResultSet();
             if (result.first()) {
-                example = new Example(result.getInt(idColumnIndex), result.getString(nameColumnIndex));
+                map = new Map(result.getInt(idColumnIndex), result.getString(mapColumnIndex));
             }
             result.close();
         }
-        return example;
+        return map;
     }
 
     /**
@@ -63,15 +70,15 @@ public abstract class ExampleDAO extends AbstractDAO {
      * @throws SQLException
      *             the SQL exception
      */
-    public static Example getExampleByName(final String name) throws SQLException {
+    public static Map getExampleByName(final String name) throws SQLException {
         final CallableStatement callStatement = prepareCall(sqlExampleByName);
-        Example example = null;
+        Map example = null;
 
         callStatement.setString(1, name);
         if (callStatement.execute()) {
             final ResultSet result = callStatement.getResultSet();
             if (result.first()) {
-                example = new Example(result.getInt(idColumnIndex), result.getString(nameColumnIndex));
+                example = new Map(result.getInt(idColumnIndex), result.getString(mapColumnIndex));
             }
             result.close();
         }
@@ -85,17 +92,31 @@ public abstract class ExampleDAO extends AbstractDAO {
      * @throws SQLException
      *             the SQL exception
      */
-    public static List<Example> getAllExamples() throws SQLException {
-        final ArrayList<Example> examples = new ArrayList<Example>();
+    public static List<Map> getAllExamples() throws SQLException {
+        final ArrayList<Map> examples = new ArrayList<Map>();
         final CallableStatement callStatement = prepareCall(sqlAllExamples);
         if (callStatement.execute()) {
             final ResultSet result = callStatement.getResultSet();
 
             for (boolean isResultLeft = result.first(); isResultLeft; isResultLeft = result.next()) {
-                examples.add(new Example(result.getInt(idColumnIndex), result.getString(nameColumnIndex)));
+                examples.add(new Map(result.getInt(idColumnIndex), result.getString(mapColumnIndex)));
             }
             result.close();
         }
         return examples;
     }
+    /*
+    public static MapInit getMapById(final int id) throws SQLException {
+        final CallableStatement callStatement = prepareCall(sqlMapsById);
+        MapInit map = null;
+        callStatement.setInt(1, id);
+        if (callStatement.execute()) {
+            final ResultSet result = callStatement.getResultSet();
+            if (result.first()) {
+                map = new MapInit(result.getInt(idColumnIndex), result.getString(mapColumnIndex));
+            }
+            result.close();
+        }
+        return map;
+    }*/
 }
